07d305d825469187fcb948060d7f7a27052567b9
Hanse Kim <procrustes5@gmail.com>
2024-05-25 14:44:28 +0900

    홈페이지 이미지 로딩

--- src/views/pages/UiHomePage.vue
+++ src/views/pages/UiHomePage.vue
@@ -1,21 +1,11 @@
 <script lang="ts" setup>
-import image1 from '@/assets/images/IMG_9295.jpeg'
-import image2 from '@/assets/images/IMG_9296.jpeg'
-import image3 from '@/assets/images/IMG_9297.jpeg'
-import image4 from '@/assets/images/IMG_9298.jpeg'
-import image5 from '@/assets/images/IMG_9299.jpeg'
-import image6 from '@/assets/images/IMG_9300.jpeg'
-import image7 from '@/assets/images/IMG_9302.jpeg'
-import image8 from '@/assets/images/IMG_9303.jpeg'
-import image9 from '@/assets/images/IMG_9304.jpeg'
-import image10 from '@/assets/images/IMG_9305.jpeg'
-
 import { useHomeStore } from '@store/homeStore.ts'
 import { storeToRefs } from 'pinia'
 import { supabase } from '@/utils/supabase'
 import Drawer from '@view/ui-elements/UiPhotoDrawer.vue'
 import Menu from '@view/ui-elements/UiMenuDrawer.vue'
 
+const photos = ref([])
 const homeStore = useHomeStore()
 const { clickedImage, isOpened } = storeToRefs(homeStore)
 const handleDrawer = (img) => {
@@ -23,7 +13,7 @@ const handleDrawer = (img) => {
   isOpened.value = true
 }
 const getHomePhotos = async (): Promise<void> => {
-  let { data, error } = await supabase.from('photo').select('*').eq('category_id', 5)
+  let { data, error } = await supabase.from('photo').select('*').eq('category_id', 6)
   photos.value = data
 }
 
@@ -33,19 +23,18 @@ onMounted(() => {
 </script>
 <template>
   <div class="home-page" ref="contentRef">
-    <el-image class="top-image" :src="image1" @click="handleDrawer(image1)"></el-image>
+    <el-image class="top-image" :src="photos[0]?.content" @click="handleDrawer(photos[0].content)"></el-image>
     <div class="middle-image">
-      <el-image class="ml-img ml-img1" :src="image2" @click="handleDrawer(image2)"></el-image>
-      <el-image class="ml-img ml-img2" :src="image3" @click="handleDrawer(image3)"></el-image>
-      <el-image class="ml-img ml-img3" :src="image4" @click="handleDrawer(image4)"></el-image>
-      <el-image class="ml-img ml-img4" :src="image5" @click="handleDrawer(image5)"></el-image>
-      <el-image class="ml-img ml-img5" :src="image6" @click="handleDrawer(image6)"></el-image>
-      <el-image class="ml-img ml-img6" :src="image10" @click="handleDrawer(image10)"></el-image>
-      <el-image class="ml-img ml-img7" :src="image7" @click="handleDrawer(image7)"></el-image>
-      <el-image class="ml-img ml-img8" :src="image9" @click="handleDrawer(image9)"></el-image>
+      <el-image
+        v-for="(photo, index) in photos.slice(1, 9)"
+        class="ml-img"
+        :class="`ml-img${index + 1}`"
+        :src="photo?.content"
+        @click="handleDrawer(photo.content)"
+      ></el-image>
     </div>
     <div class="bottom-image">
-      <el-image class="btm-img" :src="image8" @click="handleDrawer(image8)"></el-image>
+      <el-image class="btm-img" :src="photos[9]?.content" @click="handleDrawer(photos[9].content)" :lazy="true"></el-image>
     </div>
   </div>
   <Drawer></Drawer>
